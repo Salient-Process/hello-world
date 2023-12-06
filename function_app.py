ac498caa31c63dab3e8116c19de87a09a22b2b97
fpinzonperalta <fpinzonperalta@salientprocess.com>
2023-12-06 07:18:50 -0500

    Remove Hello World function

--- function_app.py
+++ function_app.py
@@ -8,39 +8,15 @@ import re
 from cf1 import runCF
 import time
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="helloWorld")
-@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
-def helloWorld(req: func.HttpRequest, msg: func.Out [func.QueueMessage]) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        msg.set(name)
-        return func.HttpResponse(f"Hello, {name}. First test {name}.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
+app = func.FunctionApp()
 
 @app.blob_trigger(arg_name="myblob", path="stage1/input/{name}",
-                               connection="AzureWebJobsStorage") 
-def functionblop(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob\n"
-                f"Name: {myblob.name}\n"
+                               connection="azureWebStorage") 
+def blop_trigger(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
-    
     connection_string = os.environ['AzureWebJobsStorage']
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
